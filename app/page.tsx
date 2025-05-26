791f24e467d479be6056dd102c20217df2a909d8
ali <ali.lee.0923@gmail.com>
2025-05-26 11:47:16 -0500

    fix font size

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import {
   Typography,
   CssBaseline,
   Container,
+  Box,
 } from "@mui/material";
 
 export default function Home() {
@@ -31,19 +32,47 @@ export default function Home() {
     <>
       <CssBaseline />
       <Container maxWidth="sm" sx={{ mt: 4 }}>
-        <Card elevation={3} sx={{ borderRadius: 3 }}>
-          <CardMedia
-            component="img"
-            height="300"
-            image={tile.imageURL}
-            alt={tile.title}
-            sx={{ borderTopLeftRadius: 12, borderTopRightRadius: 12 }}
-          />
+        <Card elevation={1} sx={{ borderRadius: "24px" }}>
+          <Box
+            sx={{
+              position: "relative",
+              width: "100%",
+              pt: "56.25%",
+              borderRadius: "24px",
+              overflow: "hidden",
+            }}
+          >
+            <CardMedia
+              component="img"
+              image={tile.imageURL}
+              alt={tile.title}
+              sx={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+              }}
+            />
+          </Box>
           <CardContent>
-            <Typography variant="h5" gutterBottom>
+            <Typography
+              variant="subtitle1"
+              sx={{ fontSize: "24px", fontWeight: 475, lineHeight: "32px" }}
+              gutterBottom
+            >
               {tile.title}
             </Typography>
-            <Typography variant="body2" color="text.secondary">
+
+            <Typography
+              variant="body2"
+              sx={{
+                fontSize: "14px",
+                fontWeight: 400,
+                lineHeight: "20px",
+              }}
+            >
               {tile.subtitle}
             </Typography>
           </CardContent>
